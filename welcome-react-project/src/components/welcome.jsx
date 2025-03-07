3a72096f8c4c64e21eac24a4ce5f88bce7349c54
CodeStripper <stripthecode@gmail.com>
2025-03-07 20:32:02 +0530

    create repo for react developer journey projects

---
+++ welcome-react-project/src/components/welcome.jsx
@@ -0,0 +1,16 @@
+import React from "react";
+
+// define welcome component 
+const Welcome = () => {
+
+    // return value
+    return(
+        <>
+            <h1>
+                Hello World!
+            </h1>
+        </>
+    )
+}
+// export welcome component
+export default Welcome;
