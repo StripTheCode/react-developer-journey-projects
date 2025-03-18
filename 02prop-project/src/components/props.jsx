43bda4a7de70ddeda8176f0990d4ed95608bff99
CodeStripper <stripthecode@gmail.com>
2025-03-18 12:18:52 +0530

    add code in codebaseD

---
+++ 02prop-project/src/components/props.jsx
@@ -0,0 +1,14 @@
+import React from "react";
+
+const Props = (props) => {
+
+
+    return(
+        <>
+            <h1>{props.welcomeMessage}</h1>
+            <p>using Props</p>
+        </>
+    )
+}
+
+export default Props;
